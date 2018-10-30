fe9fc37ea00b7ee995323f6f387dd613f431c427
olivierbretaud <olivierbretaud@hotmail.fr>
2018-10-30 19:29:26 +0100

    Moving Pacmangit add . git add . git add .  YEAHHHHHgit add . !

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 window.onload = () => {
 
-// code for press enter to have fullscreen
 
 document.addEventListener("keydown", function(e) {
     if (e.keyCode == 13) {
@@ -40,24 +39,39 @@ let map = new Image();
   map.src = './assets/image/map.jpg';
   map.onload = function() {
     context.drawImage(map, 0, 0);
+  }
+
+
+
+let zombie1= new Image();
+  zombie1.src="./assets/image/zombie1.png";
+  zombie1.onload = function() {
+    context.drawImage(zombie1, 0, 0);
+  }
+    
+let zombie2 = new Image();
+  zombie2.src = './assets/image/zombie2.png';
+  zombie2.onload = function() {
+    context.drawImage(zombie2, 0, 0);
  
   }
-//end of display map
 
 
-// hero 
+
 
 // Hero image
 let heroReady = false;
 let heroImage = new Image();
 heroImage.onload = function () {
-	context.drawImage(heroImage, 0, 0);
+	heroReady = true;
 };
-heroImage.src = "./assets/image/home-1.jpg";
+heroImage.src = "./assets/image/pacman-alone.png";
 
 // Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
+let hero = {
+  speed: 3, // movement in pixels per second
+  x: 32,
+  y: 32
 };
 
 
@@ -73,46 +87,38 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the player catches a monster
-var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+// render!
+    setInterval (function (){
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
-};
+      if (38 in keysDown) { // Player holding up
+        hero.y -= hero.speed ;
+      }
+      if (40 in keysDown) { // Player holding down
+        hero.y += hero.speed ;
+      }
+      if (37 in keysDown) { // Player holding left
+        hero.x -= hero.speed ;
+      }
+      if (39 in keysDown) { // Player holding right
+        hero.x += hero.speed ;
+      }
+      
+      if (map) {
+        context.drawImage(map, 0, 0);
+      }
+      
+      if (heroReady){
+        context.drawImage(heroImage, hero.x, hero.y);
+      }
+      
+      // if (zombie1) {
+      //   context.drawImage(zombie1, 250, 250);
+      // }
+      
+      // if (zombie2) {
+      //   context.drawImage(zombie2, 300, 300);
+      // }
+  }, 1000/30)
 
-// Update game objects
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-  }
-
-  	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
-	}
-};
-
-
-
-// end of the game code
 
 }
-
