70757c5388d59058ad4c1efe336f5f0c9ddcb014
olivierbretaud <olivierbretaud@hotmail.fr>
2018-10-30 18:24:29 +0100

    test

--- js/index.js
+++ js/index.js
@@ -45,6 +45,72 @@ let map = new Image();
 //end of display map
 
 
+// hero 
+
+// Hero image
+let heroReady = false;
+let heroImage = new Image();
+heroImage.onload = function () {
+	context.drawImage(heroImage, 0, 0);
+};
+heroImage.src = "./assets/image/home-1.jpg";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+
+
+
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reset the game when the player catches a monster
+var reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+var update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+  }
+
+  	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+
 
 // end of the game code
 
